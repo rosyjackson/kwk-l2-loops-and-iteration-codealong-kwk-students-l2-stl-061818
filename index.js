91de3b07f38dae288a6fb031febff57e196bcaf1
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 16:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function tailsNeverFails(){
   let tails = 0;
   
-  while (Math.random( ) >= 0.0000000000000000000000000000000001 ){
+  while (Math.random( ) >= 0.0000000000001 ){
     tails++
   }
   
