88720c976092599ac6291703fc2e1f3277394789
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 16:33:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function tailsNeverFails(){
   let tails = 0;
   
-  while (Math.random()<=0.5 )
+  while (Math.random()<=0.5 ){
+    tails++
+  }
+  
+  return `You got ${tails} tails in a row!`;
 }
